db46c3745755818a63415173f186b89b1d343c94
Richard Möhn <richard.moehn@posteo.de>
2015-01-30 11:18:37 +0100

    Remove some reloaded stuff, add basic fns and data

--- dev/user.clj
+++ dev/user.clj
@@ -11,42 +11,50 @@
    [clojure.string :as str]
    [clojure.test :as test]
    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
-   [baustellen]))
+   [baustellen :refer :all]))
 
-(def system
-  "A Var containing an object representing the application under
-  development."
-  nil)
+(def skill-cost {:walls 30
+                 :roof 20
+                 :plumbing 10})
 
-(defn init
-  "Creates and initializes the system under development in the Var
-  #'system."
-  []
-  ;; TODO
-  )
+(def sites [{:payoff 300
+             :name "Sporthalle"
+             :skills {:walls 3
+                      :roof 6
+                      :plumbing 1}
+             :location [4 4]}
+            {:payoff 600
+             :name "Schwimmhalle"
+             :skills {:walls 2
+                      :roof 0
+                      :plumbing 8}
+             :location [3 1]}
+            {:payoff 150
+             :name "Einfamilienhaus"
+             :skills {:walls 5
+                      :roof 5
+                      :plumbing 5}
+             :location [7 1]}])
 
-(defn start
-  "Starts the system running, updates the Var #'system."
-  []
-  ;; TODO
-  )
+(def agents [{:name "Maurer 1"
+              :skill :walls
+              :capacity 10
+              :location [1 5]}
+             {:name "Maurer 2"
+              :skill :walls
+              :capacity 7
+              :location [5 4]}
+             {:name "Dachdecker 1"
+              :skill :roof
+              :capacity 15
+              :location [0 2]}
+             {:name "Klempner 1"
+              :skill :plumbing
+              :capacity 8
+              :location [3 2]}
+             {:name "Klempner 2"
+              :skill :plumbing
+              :capacity 8
+              :location [8 2]}])
 
-(defn stop
-  "Stops the system if it is currently running, updates the Var
-  #'system."
-  []
-  ;; TODO
-  )
 
-(defn go
-  "Initializes and starts the system running."
-  []
-  (init)
-  (start)
-  :ready)
-
-(defn reset
-  "Stops the system, reloads modified source files, and restarts it."
-  []
-  (stop)
-  (refresh :after 'user/go))
--- src/baustellen.clj
+++ src/baustellen.clj
@@ -1,3 +1,17 @@
 (ns baustellen)
 
+(defn distance [p1 p2]
+  (Math/sqrt
+    (apply +
+           (map (fn [c1 c2] (Math/pow (- c1 c2) 2))
+                p1 p2))))
 
+(defn cost
+  "Returns the cost for transporting cnt units of skill over a dist. The basis
+  cost for transporting one unit of skill over one unit of distance is taken
+  from skill-costs.
+
+  Currently the cost increases linearly with the distance and squareroot-ly with
+  the number of skill-units transported."
+  [dist skill cnt skill-costs]
+  (* dist (Math/sqrt cnt) (get skill-costs skill)))

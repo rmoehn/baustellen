ea98da22ec6e932b447086f7a26e2db747c1523a
Richard Möhn <richard.moehn@posteo.de>
2015-02-06 13:23:23 +0100

    Add some quick and dirty visualization

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [amalloy/ring-buffer "1.0"]]
+                 [amalloy/ring-buffer "1.0"]
+                 [rhizome "0.2.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
                    :source-paths ["dev"]}})
